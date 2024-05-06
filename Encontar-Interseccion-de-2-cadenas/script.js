16a2a815a8698241cf92d505de30d16eef69442c
Patricio Avila <152195893+Avila-Pato@users.noreply.github.com>
2024-05-06 13:03:10 -0400

    Add files via upload

---
+++ Encontar-Interseccion-de-2-cadenas/script.js
@@ -0,0 +1,19 @@
+// Identificando caractares entre Ambos osea cuales tienen en comun
+
+function intersectionString (str1, str2) {
+    let intersection = '';
+    const seenChars = {};
+
+    for (const element of str1 ) {
+        const char = element;
+
+        if (str2.includes(char) && !seenChars[char]) {
+            intersection += char;
+            seenChars[char] = true;
+    
+    }
+}
+return intersection;
+}
+
+console.log(intersectionString('maicol', 'patricio'));
