f2c90d04c0dd773c0121162a04a6d3a158233c4b
Patricio Avila <152195893+Avila-Pato@users.noreply.github.com>
2024-05-09 16:57:10 -0400

    Update index.js

--- Ejecicio-1/index.js
+++ Ejecicio-1/index.js
@@ -1,11 +1,15 @@
 function findFirstRepeated(gifts) {
+    // se crea la funcion  findFirstRepeated con un parametro llamado 'gifts'
     const objeto = {};
-
+    // se crea un objeto con un array vacio
 for(const value of gifts){
+    // se inicia un bucle que itenera sobre el parametro gifts
   if (objeto[value]) {
-    return value
+    // si  dentro del regalo actual 'value' se encuentra en objeto  significa que encontro el regalo repetido, devolviendo ese regalo como el primer regalo encontrado
+    return value 
   } else {
     objeto[value] = true
+    //  si el regalo no se repite significa que es la primera vez que lo encontra a;adiensose el valor true , marcando como visto el objeto para futuras iteraciones
   }
 }
 
