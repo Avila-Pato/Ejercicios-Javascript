958d6a25b3dd60ecaa51ca8a936b6fea56a2845b
Patricio Avila <152195893+Avila-Pato@users.noreply.github.com>
2024-05-09 21:04:29 -0400

    Update index.js

--- Ejecicio-1/index.js
+++ Ejecicio-1/index.js
@@ -11,7 +11,7 @@ for(const value of gifts){
       // el primer regalo encontrado
   } else {
     objeto[value] = true
-    //  si el regalo no se repite  a;adiensose el valor true  significa que es la primera vez que lo encontra , marcando como visto el objeto para futuras iteraciones
+    // indicador que encuentra el bucle for itineradnoe l array gift  
   }
 }
 
